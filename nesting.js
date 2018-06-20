d8ed94bef5808cdc9da9175d06d43bd10f7ade04
jdaniel <jdricher2017@gmail.com>
2018-06-20 18:54:55 -0500

    nesting complete

--- nesting.js
+++ nesting.js
@@ -52,111 +52,118 @@ var employees = [
 
 function employeeUpdater() {
   for (let i = 0; i < employees.length; i++) {
-    if (employees.firstName === "Theo") {
-      delete (employees[2].firstName)
-      delete (employees[2].lastName)
-      delete (employees[2].email)
-      delete (employees[2].department)
-    } else if (employees.firstName === "Lorie") {
-      employees[4].department = "HR"
+    if (employees[i].firstName === "Theo") {
+      employees.splice(i, 1);
+    } else if (employees[i].firstName === "Lorie") {
+      employees[i].department = "HR"
     }
-    return employees;
   }
+  return employees;
+}
 
 
 
-  ////////// PROBLEM 2 //////////
+////////// PROBLEM 2 //////////
 
-  // Do not edit the code below.
-  var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
-  // Do not edit the code above.
+// Do not edit the code below.
+var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
+// Do not edit the code above.
 
-  /*
-    The array above represents IDs tied to reported workplace accidents. 
-    An employee accidentally entered in duplicates to array, making it look as though there are more accidents this year than there actually are.
-      1. Write a function called 'removeDuplicates' that will remove all duplicate entries in the workplaceAccidents array.
-      2. Use nested for loops to check for duplicate numbers, and then remove the duplicates from the array.
-      3. Return the updated array.
-  */
+/*
+  The array above represents IDs tied to reported workplace accidents. 
+  An employee accidentally entered in duplicates to array, making it look as though there are more accidents this year than there actually are.
+    1. Write a function called 'removeDuplicates' that will remove all duplicate entries in the workplaceAccidents array.
+    2. Use nested for loops to check for duplicate numbers, and then remove the duplicates from the array.
+    3. Return the updated array.
+*/
 
-  //Code Here
-
-
-
-  ////////// PROBLEM 3 //////////
-
-  // Do not edit the code below.
-  var cat = {
-    name: 'Fluffy',
-    catFriends: [
-      {
-        name: 'Grumpy',
-        activities: ['be grumpy', 'eat food']
-      },
-      {
-        name: 'Lazy Bones',
-        activities: ['sleep', 'pre-sleep naps']
+let removeDuplicates = arr => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        arr.splice(i, 1);
+        i--;
       }
-    ]
+    }
   }
-  // Do not edit the code above.
-
-  /*
-    Fluffy has two friends, Grumpy and Lazy Bones. 
-      1. Assign the value of Grumpy's 2nd activity to the grumpyActivity variable below.
-      2. Assign fluffy2ndFriend the name of Fluffy's 2nd friend.
-  */
-
-  //Code Here
-  var grumpyActivity;
-  var fluffy2ndFriend;
+  return arr;
+};
 
 
+////////// PROBLEM 3 //////////
 
-  ////////// PROBLEM 4 //////////
+// Do not edit the code below.
+var cat = {
+  name: 'Fluffy',
+  catFriends: [
+    {
+      name: 'Grumpy',
+      activities: ['be grumpy', 'eat food']
+    },
+    {
+      name: 'Lazy Bones',
+      activities: ['sleep', 'pre-sleep naps']
+    }
+  ]
+}
+// Do not edit the code above.
 
-  // Do not edit the code below.
-  var myCar = {
-    make: 'Toyota',
-    model: 'Corolla',
-    year: 1992,
-    accidents: [
-      {
-        date: '3/15/93',
-        damage: '$5,000',
-        atFaultForAccident: true
-      },
-      {
-        date: '7/4/98',
-        damage: '$2,200',
-        atFaultForAccident: true
-      },
-      {
-        date: '6/22/99',
-        damage: '$7,900',
-        atFaultForAccident: true
-      }
-    ]
-  }
-  // Do not edit the code above.
+/*
+  Fluffy has two friends, Grumpy and Lazy Bones. 
+    1. Assign the value of Grumpy's 2nd activity to the grumpyActivity variable below.
+    2. Assign fluffy2ndFriend the name of Fluffy's 2nd friend.
+*/
 
-  /*
-    Above is some information about my car. As you can see, I am not the best driver.
-    I have caused a few accidents.
-    Please update this driving record so that I can feel better about my driving skills.
-      1. Write a function called recordCleaner.
-      2. Loop over the accidents array.
-      3. Change atFaultForAccident from true to false.
-  */
-
-  //Code Here
+//Code Here
+var grumpyActivity;
+var fluffy2ndFriend;
 
 
 
-  ////////// PROBLEM 5 //////////
+////////// PROBLEM 4 //////////
 
-  // Do not edit the code below.
-  var numsArr = [[1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
+// Do not edit the code below.
+var myCar = {
+  make: 'Toyota',
+  model: 'Corolla',
+  year: 1992,
+  accidents: [
+    {
+      date: '3/15/93',
+      damage: '$5,000',
+      atFaultForAccident: true
+    },
+    {
+      date: '7/4/98',
+      damage: '$2,200',
+      atFaultForAccident: true
+    },
+    {
+      date: '6/22/99',
+      damage: '$7,900',
+      atFaultForAccident: true
+    }
+  ]
+}
+// Do not edit the code above.
+
+/*
+  Above is some information about my car. As you can see, I am not the best driver.
+  I have caused a few accidents.
+  Please update this driving record so that I can feel better about my driving skills.
+    1. Write a function called recordCleaner.
+    2. Loop over the accidents array.
+    3. Change atFaultForAccident from true to false.
+*/
+
+//Code Here
+
+
+
+////////// PROBLEM 5 //////////
+
+// Do not edit the code below.
+var numsArr = [[1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
 // Do not edit the code above.
 
 /*
